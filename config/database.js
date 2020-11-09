e8c3f1ac3a8ad0e28ae21c68bea8225fae3b4244
danielsalum <69260643+danielsalum@users.noreply.github.com>
2020-11-09 15:18:17 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 console.log('(Dentro de databse.js)DATABASE_URL==>>', process.env.DATABSE_URL);
 module.exports = {
-  url: process.env.DATABASE_URL,
+  //url: process.env.DATABASE_URL,
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "docker",
-  database: "webproj2",
+  host: "ec2-54-237-155-151.compute-1.amazonaws.com",
+  username: "dytaxrfqgfbgif",
+  password: "2debdd27679170f56124250b03f762cae4119a5f5217672ae4fbbf5549df5fb0",
+  database: "d794447f4d1f1k",
   define: {
     timestamps: true,
     underscored: true,
